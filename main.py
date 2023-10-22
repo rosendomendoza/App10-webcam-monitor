40293e986535a3770ad554ff20648c3984874104
ROSENDO MENDOZA <rosendo.arturo@gmail.com>
2023-10-22 13:07:00 +0200

    Day 36: Capture video from webcam

--- main.py
+++ main.py
@@ -1,1 +1,16 @@
+import time
 import cv2
+
+video = cv2.VideoCapture(0)
+time.sleep(1)
+
+while True:
+    check, frame = video.read()
+    cv2.imshow("My Video", frame)
+
+    key = cv2.waitKey(1)
+
+    if key == ord("q"):
+        break
+
+video.release()
