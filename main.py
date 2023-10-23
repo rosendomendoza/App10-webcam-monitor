f5e82ba7f5710adea1bb2ee625dc05008482ed52
ROSENDO MENDOZA <rosendo.arturo@gmail.com>
2023-10-23 17:11:47 +0200

    Day 37: improve the application using threads in the send and clean functions

--- main.py
+++ main.py
@@ -5,6 +5,13 @@ from datetime import datetime
 import cv2
 from emailing import send_email
 import shutil
+from threading import Thread
+
+def clean_folder():
+    images = glob.glob("images/capture/*.png")
+    for image in images:
+        os.remove(image)
+
 
 video = cv2.VideoCapture(0)
 time.sleep(1)
@@ -31,7 +38,7 @@ while True:
     # cv2.imshow("My Video", delta_frame)
     thresh_frame = cv2.threshold(delta_frame, 60, 255,
                                  cv2.THRESH_BINARY)[1]
-    #cv2.imshow("My Video", thresh_frame)
+
     dil_frame = cv2.dilate(thresh_frame, None, iterations=2)
 
     contours, check = cv2.findContours(dil_frame, cv2.RETR_EXTERNAL,
@@ -63,12 +70,18 @@ while True:
         # Store image to send
         img_path = f"images/send/{img_name}"
         shutil.copy(f"{image2send}", img_path)
-        # Remove the unused images
-        shutil.rmtree("images/capture")
-        os.makedirs("images/capture/")
 
-        # Send the notification with the selected image
-        send_email(img_path)
+        # Send the notification with the selected image in a Thread
+        send_thread = Thread(target=send_email, args=(img_path, ))
+        send_thread.daemon = True
+        send_thread.start()
+        # send_email(img_path)
+
+        # Clean the folder in a thread
+        clean_thread = Thread(target=clean_folder)
+        clean_thread.daemon = True
+        clean_thread.start()
+        # clean_folder()
         count = 1
 
     cv2.imshow("My Video", frame)
